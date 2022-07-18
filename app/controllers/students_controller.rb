a5976cbc99d36b606c5d7ab86ad942909ca1a7ad
Allie LeFever <alefever39@gmail.com>
2022-07-18 08:17:12 -0600

    commit tree 03567213ef098253cc1a5a67217b09bd3cc5b41f

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
-
   def index
-    students = Student.all
+    if params[:name]
+      students = Student.find_by(first_name: params[:name])
+      students = Student.find_by(last_name: params[:name]) if (!students)
+    else
+      students = Student.all
+    end
     render json: students
   end
 
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
 end
